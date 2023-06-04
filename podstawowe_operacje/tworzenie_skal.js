5cfe29835a1a247a2c096d036d311303d0ebdba8
Novichi <92579061+Novichi@users.noreply.github.com>
2023-06-04 02:11:46 +0200

    Update tworzenie_skal.js

--- podstawowe_operacje/tworzenie_skal.js
+++ podstawowe_operacje/tworzenie_skal.js
@@ -3,6 +3,9 @@ var scale = d3.scaleLinear()
   .domain([0, 10])  // dane wejściowe
   .range([0, 100]);  // dane wyjściowe
 
+var point = 3
+
 // Obliczamy wartość 5 przy użyciu skali i wyświetlamy wynik na stronie
-var output = scale(5);  // powinno zwrócić 50
+var output = scale(point);  // powinno zwrócić 50
+d3.select('#input').text("Wynik: " + point);
 d3.select("#output").text("Wynik: " + output);
